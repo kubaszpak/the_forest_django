8ef8fed5bb298c762b9907852e0d6e5c8f05e8fc
kubaszpak <jakub.szpak.it@gmail.com>
2021-02-17 00:32:47 +0100

    Figured out images

--- src/pages/src/components/App.js
+++ src/pages/src/components/App.js
@@ -7,7 +7,61 @@ export default class App extends Component {
   }
 
   render() {
-    return <div>Testing this shit</div>;
+    return (
+    <>
+    <div class = "parent-container">
+    <div class="animated-container">
+    <div class = "board">
+      <div class="box" id="first-box">
+        <div class="tree">
+          <img src="static/images/building1.svg" alt="My image"/>
+          </div>
+      </div>
+      <div class="box" id="second-box">
+        <div class="tree">
+          <img src="static/images/building1.svg" alt="My image"/>
+      </div>
+      </div>
+      <div class="box" id="third-box">
+        <div class="tree">
+          <img src="static/images/building1.svg" alt="My image"/>
+        </div>
+      </div>
+      <div class="box" id="fourth-box">
+        <div class="tree">
+          <img src="static/images/building1.svg" alt="My image"/>
+        </div>
+      </div>
+      <div class="box" id="fifth-box">
+        <div class="tree">
+          <img src="static/images/building1.svg" alt="My image"/>
+        </div>
+      </div>
+      <div class="box" id="sixth-box">
+        <div class="tree">
+          <img src="static/images/building1.svg" alt="My image"/>
+        </div>
+      </div>
+      <div class="box" id="seventh-box">
+        <div class="tree">
+          <img src="static/images/building1.svg" alt="My image"/>
+        </div>
+      </div>
+      <div class="box" id="eighth-box">
+        <div class="tree">
+          <img src="static/images/building1.svg" alt="My image"/>
+        </div>
+      </div>
+      <div class="box" id="ninth-box">
+        <div class="tree">
+          <img src="static/images/building1.svg" alt="My image"/>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  </>
+  );
   }
 }
 
