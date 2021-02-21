03fa14cf0913069213f97bcb38755e5522c04c4c
kubaszpak <jakub.szpak.it@gmail.com>
2021-02-21 01:08:59 +0100

    Managed to make react work

--- src/pages/src/components/App.js
+++ src/pages/src/components/App.js
@@ -1,50 +1,25 @@
-import React, { Component } from "react";
-import { render, Redirect } from "react-dom";
+import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import HomePage from "./HomePage.js";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      roomCode: null,
-    };
-  }
+function App() {
 
-  async componentDidMount() {
-    fetch("/api/user-in-room")
-      .then((response) => response.json())
-      .then((data) => {
-        if (response.ok) {
-          this.setState({
-            roomCode: data.code,
-          });
-        }
-      });
-  }
-
-  handleCreateNewRoom() {
-    const requestOptions = {
-      method: "POST",
-      header: { ContentType: "application/json" },
-    };
-    fetch("/api/create-room", requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        if (response.ok) {
-          this.props.history.push(`/room/${data.code}`);
-        }
-      });
-  }
-
-  render() {
-    return this.state.roomCode ? (
-      <Redirect to={`/room/${this.state.roomCode}`} />
-    ) : (
-      () => {
-        this.handleCreateNewRoom();
-      }
-    );
-  }
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={HomePage}/>
+        <Route path = "/room/:roomCode" render = {
+          () => {
+            this.renderNewRoom();
+          }
+        }/>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+export default App;
